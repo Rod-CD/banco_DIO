c56d67122782415bbb3ff09d6bf0b70dd11b5722
Rod-CD <rodcardal9@gmail.com>
2024-09-11 10:36:12 -0300

    Passando a utilizar funções

--- banco.py
+++ banco.py
@@ -1,12 +1,31 @@
 #banco
 def Deposito(saldo):
-    ...
+    aux_deposito = float(input('quanto deseja depositar? '))
+    saldo += aux_deposito
+    historico.append(str(aux_deposito))
 
-def Saque():
-    ...
+    return saldo
+
+def Saque(limite_saque, saldo):
+    if limite_saque > 0:
+        aux_saque = float(input('quanto deseja retirar? '))
+        if 500 >= aux_saque > 0:
+            saldo -= aux_saque
+            historico.append(f'-{str(round(aux_saque,2))}')
+            limite_saque -= 1
+        else:
+            print('vc digitou um valor maior que o limite de 500 ou um valor invalido.')
+    else:
+        print("Voce já atingiu o limite de saque diario")
+
+    return limite_saque, saldo
 
 def Extrato():
-    ...
+    if historico == []:
+        print('Não foram realizadas movimentações')
+    else:
+        print(f'seu saldo é de: {saldo:.2f}, seu historico:\n{"\n".join(historico)}')
+        
 
 saldo = 0
 historico = []
@@ -26,29 +45,12 @@ while sair == False:
 
     opcao = input("O que deseja fazer? ")
     if opcao == '1':
-        aux_deposito = float(input('quanto deseja depositar? '))
-        saldo += aux_deposito
-        historico.append(str(aux_deposito))
-
+        saldo = Deposito(saldo)
     elif opcao == '2':
-        if limite_saque > 0:
-            aux_saque = float(input('quanto deseja retirar? '))
-            if 500 >= aux_saque > 0:
-                saldo -= aux_saque
-                historico.append(f'-{str(round(aux_saque,2))}')
-            else:
-                print('vc digitou um valor maior que o limite de 500 ou um valor invalido.')
-            limite_saque -= 1
-        else:
-            print("Voce já atingiu o limite de saque diario")
+        limite_saque, saldo = Saque(limite_saque, saldo)
     elif opcao == '3':
-        if historico == []:
-            print('Não foram realizadas movimentações')
-        else:
-            print(f'seu saldo é de: {saldo:.2f}, seu historico:\n{"\n".join(historico)}')
-        
+        Extrato()
     elif opcao == '4':
-        print('selecionou 4')
         sair = True
     else:
         print('opção invalida, escolha uma opcao valida')
