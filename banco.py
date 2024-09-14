ba6e55c0c0844e742e5083ebe8b11433d6989f47
Rod-CD <rodcardal9@gmail.com>
2024-09-14 19:56:10 -0300

    Correções de limite diario e adição de horario das transações

--- banco.py
+++ banco.py
@@ -1,25 +1,32 @@
-#banco
-def Deposito(saldo, limite_transacoes):
+import datetime
+import pytz
+
+def Deposito(saldo, limite_transacoes, historico):
     if limite_transacoes > 0:
         aux_deposito = float(input('quanto deseja depositar? '))
         saldo += aux_deposito
-        historico.append(str(aux_deposito))
+        horario = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
+        historico.append(f'{str(aux_deposito)}     as {horario.strftime("%H:%M %d/%m/%Y")}')
         limite_transacoes -= 1
     else:
         print("Voce já atingiu o limite de transações diario")
 
-    return saldo, limite_transacoes
+    return saldo, limite_transacoes, historico
 
-def Saque(limite_saque, saldo, limite_transacoes):
-    if limite_saque > 0 and limite_transacoes >0: 
+def Saque(LIMITE_PARA_SAQUE, limite_saque, saldo, limite_transacoes):
+    if limite_saque > 0 and limite_transacoes > 0: 
         aux_saque = float(input('quanto deseja retirar? '))
-        if 500 >= aux_saque > 0:
-            saldo -= aux_saque
-            historico.append(f'-{str(round(aux_saque,2))}')
-            limite_saque -= 1
-            limite_transacoes -= 1
+        if LIMITE_PARA_SAQUE >= aux_saque > 0:
+            if saldo >= aux_saque:
+                saldo -= aux_saque
+                horario = datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))
+                historico.append(f'-{str(round(aux_saque,2))}     as {horario.strftime("%H:%M %d/%m/%Y")}')
+                limite_saque -= 1
+                limite_transacoes -= 1
+            else:
+                print('Saldo insuficiente.')
         else:
-            print('vc digitou um valor maior que o limite de 500 ou um valor invalido.')
+            print(f'vc digitou um valor maior que o limite de {LIMITE_PARA_SAQUE} ou um valor invalido.')
     else:
         if limite_saque == 0:
             print("Voce já atingiu o limite de transações diarias")
@@ -38,6 +45,7 @@ def Extrato():
 saldo = 0
 historico = []
 sair = False
+LIMITE_PARA_SAQUE = 500
 limite_saque = 3
 limite_transacoes = 10
 
@@ -54,9 +62,9 @@ while sair == False:
 
     opcao = input("O que deseja fazer? ")
     if opcao == '1':
-        saldo = Deposito(saldo)
+        saldo, limite_transacoes, historico = Deposito(saldo, limite_transacoes, historico)
     elif opcao == '2':
-        limite_saque, saldo = Saque(limite_saque, saldo)
+        limite_saque, saldo, limite_transacoes = Saque(LIMITE_PARA_SAQUE, limite_saque, saldo, limite_transacoes)
     elif opcao == '3':
         Extrato()
     elif opcao == '4':
